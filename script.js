c51997b95d89f813e3a5597dec2950a255d3ba10
James Kyle <me@thejameskyle.com>
2013-04-16 21:25:08 -0400

    Add Main Script File

---
+++ script.js
@@ -0,0 +1,14 @@
+(function() {
+  var recovery_widget = document.createElement('div'), content = {};
+
+  content.title = "Title Of Disaster";
+  content.text = "This is some content next to an image. This is what you need to know. This is some other stuff you need to know.";
+  content.imgSrc = "http://placehold.it/93";
+  content.link = "#";
+
+  recovery_widget.setAttribute('class', 'recovery_widget');
+  recovery_widget.innerHTML =
+    '<div class="media_block"><img class="float_left" src="' + content.imgSrc + '"/><div class="content"><h5><small>Happening Now:</small></h5><h1>' + content.title + '</h1><p>' + content.text + '</p><p><a href="' + content.link + '">Read More &rarr;</a></p></div></div>';
+
+  document.body.insertBefore(recovery_widget);
+})();
