02c8cda8133ca216555b1694abbc5d6bc6df749e
Emily Dirsh <edirsh@redhat.com>
2013-04-18 21:52:11 -0400

    Clean up widget creator javascript
    Pulled configurable elements into a config.json file, put all on-page
    javascript into an external file, and somewhat modularized the functions
    specific to this page.

---
+++ widget_creator/js/script.js
@@ -0,0 +1,28 @@
+var iebs =  {
+        //Populate select options
+        add_opts: function(select, opts) {
+		$.each(opts, function(i, val) {
+			$(select).append('<option value="' + i + '">' + val + '</option>');
+		});
+        },
+        //Generate script
+        get_script: function(codes, url) {
+		return '&lt;script type="text/javascript" src="' + url + '?' + codes + '"&gt;&lt;/script&gt;';
+        },
+	update_script: function(select, text, url) {
+		var codes = $(select).val();
+		console.log(select, codes);
+		if (codes) {
+			$(text).html(this.get_script(codes.join('+'), url));
+		} else {
+			$(text).html('');
+		}
+	}
+};
+
+(function() {
+	iebs.add_opts('#cities', config.cities);
+	$("#cities").chosen().change(function() {
+		iebs.update_script(this, '#code', config.url);
+	});
+})();
