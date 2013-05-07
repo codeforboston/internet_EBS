3a91ac566a5895888a1aeb962bfb1be54cd16921
Aaron Sikes <jingadjanga@gmail.com>
2013-05-07 18:36:01 -0400

    Actually rendering the snippet now. A bit of a step backwards, since there's no longer support for multiple cities, and the status only alternates again. BUT I think the templating approach is a lot better and it won't be too tough to add that back in. -Aaron

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,49 +1,21 @@
-/*
- * GET home page.
- */
+
+var fs = require('fs');
+var path = require('path');
+var jade = require('jade');
 
 exports.index = function (req, res) {
 	res.render('index', { title: 'Express' });
 };
 
-exports.direct = function(request, response){
-	var content = {
-		title: "Title Of Disaster",
-		text: "This is some content next to an image. This is what you need to know. This is some other stuff you need to know.",
-		image: "http://placehold.it/93",
-		link: "#"
-	}
-	response.render('banner/page', {content: content});
-}
+exports.render_snippet = require('./render-snippet.js');
 
 var statuses = {
 	simcity: false,
 	othercity: false
 };
 
-exports.banner = function (request, response) {
-	var requested_cities = parse_comma_list(request.query.cities);
-	console.log('reqcit'+requested_cities);
-	for (var city in requested_cities) {
-		city = requested_cities[city];
-		console.log('city'+city);
-		console.log('stat'+statuses[city])
-		if (statuses[city]) {
-			return response.sendfile('public/cities/city.js');
-		}
-	}
-
-	response.send(200);
-};
-
-
 exports.admin = function (request, response) {
 	var city = request.params.city;
 	statuses[city] = !statuses[city];
 	response.render('admin', {city: city, status: statuses[city]});
 }
-
-function parse_comma_list(comma_list) {
-	return comma_list.split(',');
-}
-
---
+++ backend/routes/render-snippet.js
@@ -0,0 +1,35 @@
+
+var fs = require('fs');
+var path = require('path');
+
+var jade = require('jade');
+var mustache = require('mustache');
+
+
+var jade_templ_path = path.join(__dirname, '..','templates', 'widget.jade');
+var jade_text = fs.readFileSync(jade_templ_path);
+var jade_fn = jade.compile(jade_text);
+
+var mustache_templ_path = path.join(__dirname, '..', 'templates','script.mustache');
+var mustache_text = fs.readFileSync(mustache_templ_path, 'utf8');
+var mustache_fn = mustache.compile(mustache_text);
+
+var content = {
+	title: "Title Of Disaster",
+	text: "This is some content next to an image. This is what you need to know. This is some other stuff you need to know.",
+	image: "http://placehold.it/93",
+	link: "#"
+}
+
+var emergency = true;
+
+module.exports = function(request, response){
+	emergency = !emergency;
+	if (! emergency) return response.send(204);
+
+	var html = jade_fn({content: content});
+	var snippet = mustache_fn({html: html});
+	response.send(snippet);
+};
+
+
