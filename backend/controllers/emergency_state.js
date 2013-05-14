99d3c159dee00993d0fbbce2e8aca534373ec9c8
Aaron Sikes <jingadjanga@gmail.com>
2013-05-14 18:29:05 -0400

    Added a simple timestamp when someone submits emergency info -Aaron

--- backend/controllers/emergency_state.js
+++ backend/controllers/emergency_state.js
@@ -1,38 +1,12 @@
-var boston_content = {
-	city: 'Boston',
-	title: "Title Of Disaster",
-	text: "This is some content next to an image. This is what you need to know. This is some other stuff you need to know.",
-	image: "http://placehold.it/93",
-	link: "#"
-};
-
-var simcity_content = {
-	city: 'SimCity',
-	title: "Title Of Disaster",
-	text: "This is some content next to an image. This is what you need to know. Sometimes you need to know some other things.",
-	image: "http://placehold.it/93",
-	link: "#"
-};
 
 
 var state = {};
-module.exports.toggle = function toggle(city){
-	if (city === 'boston'){
-		 if (state.boston) return state.boston = false;
-		 else return state.boston = boston_content;
-	}
-	if (city === 'simcity'){
-		if (state.simcity) return state.simcity = false;
-		else return state.simcity = simcity_content;
-	}
-	console.log('nope');
-	state[city] = !state[city];
-};
 
 module.exports.set = function set(city, val){
 	if (typeof val === 'object'){
 		val.city = city;
 		val.image = "http://placehold.it/93"
+		val.last_updated = new Date().toString()
 	}
 	state[city] = val;
 }
